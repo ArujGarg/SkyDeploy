b60b9b66b289f0f356fe7c67d8c42917c3370c07
ArujGarg <arujgarg04@gmail.com>
2026-06-19 23:43:33 +0530

    get and post api

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,58 @@
 import express from "express";
+import { prisma } from "./db/prisma.js";
 
 const app = express();
 
-app.listen(3000, () => {
+app.use(express.json());
+
+app.get("/api/deployments/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deployment = await prisma.deployment.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!deployment) {
+      return res.status(404).json({
+        message: "Deployment not found",
+      });
+    }
+
+    return res.json(deployment);
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Failed to fetch deployment",
+    });
+  }
+});
+
+app.post("/api/deployments", async (req, res) => {
+  try {
+    const { githubRepoUrl, branch } = req.body;
+
+    const deployment = await prisma.deployment.create({
+      data: {
+        githubRepoUrl,
+        branch: branch || "main",
+        status: "QUEUED",
+      },
+    });
+
+    res.status(201).json(deployment);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Failed to create deployment",
+    });
+  }
+});
+
+app.listen(3001, () => {
   console.log("Server is running on port 3000");
 });
